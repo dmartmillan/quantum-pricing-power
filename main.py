b4f644fddae3863318ec6c3d26ca99d42841965f
David <dmartinezmillan@hotmail.com>
2019-09-13 01:23:09 +0200

    Main program

---
+++ main.py
@@ -0,0 +1,15 @@
+import argparse
+import sys
+
+parser = argparse.ArgumentParser(prog='Program')
+
+def main():
+    args = parser.parse_args()
+    print("Hello quantum!")
+    val = input("Enter your value: ") 
+    while (val != str("call") and val != str("put")):
+        val = input("Oops! The value was wrong. Try again...\nEnter your value: ") 
+    else: print(val)
+
+if __name__== "__main__":
+    main()
